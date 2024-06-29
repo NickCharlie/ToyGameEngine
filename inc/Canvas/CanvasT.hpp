56a63d8d925f94b9a4c5a962f16478d198031fbc
NickMo <max318515692@gmail.com>
2024-06-29 16:33:34 +0800

    修复了遗留的CanvasT类报错问题

--- inc/Canvas/CanvasT.hpp
+++ inc/Canvas/CanvasT.hpp
@@ -12,23 +12,21 @@ namespace ToyGameEngine {
 		class ICanvasT : public ICanvas_Impl, public QWidget {
 			Q_OBJECT
 		public:
-			QPainter* m_painter = new QPainter(this);
+			QPainter* m_painter;
 
-			ICanvas(QPainter *painter) : m_painter(painter){}
+			ICanvasT(QPainter* painter) : m_painter(painter) {}
 
 			// 绘图事件
             void paintEvent(QPaintEvent *event) override;
 
 			void draw(const Shape &shape);
 
-			void draw_graph();
+			void draw_graph() override;
 
 		private:
             GeometryGroup group;
 
             void init();
-
-            void draw_graph();
 
             void draw_cache();
 
@@ -36,53 +34,53 @@ namespace ToyGameEngine {
 
         protected:
 
-            void mousePressEvent(QMouseEvent *event);
+            void mousePressEvent(QMouseEvent *event) override;
 
-            void mouseReleaseEvent(QMouseEvent *event);
+            void mouseReleaseEvent(QMouseEvent *event) override;
 
-            void mouseMoveEvent(QMouseEvent *event);
+            void mouseMoveEvent(QMouseEvent *event) override;
 
-            void wheelEvent(QWheelEvent *event);
+            void wheelEvent(QWheelEvent *event) override;
 
-            void mouseDoubleClickEvent(QMouseEvent *event);
+            void mouseDoubleClickEvent(QMouseEvent *event) override;
 
-            void resizeEvent(QResizeEvent *event);
+            void resizeEvent(QResizeEvent *event) override;
 
 		};
 
 		template <>
 		void ICanvasT<Math::Geometry::Polygon>::draw(const Math::Geometry::Polygon& polygon) {
-		
+
 		}
 
 		template <>
 		void ICanvasT<Math::Geometry::Line>::draw(const Math::Geometry::Line& line) {
-		
+
 		}
 
 		template <>
 		void ICanvasT<Math::Geometry::AABBRect>::draw(const Math::Geometry::AABBRect& rect) {
-		
+
 		}
 
 		template <>
 		void ICanvasT<Math::Geometry::Bezier>::draw(const Math::Geometry::Bezier& bezier) {
-		
+
 		}
 
 		template <>
 		void ICanvasT<Math::Geometry::Circle>::draw(const Math::Geometry::Circle& circle) {
-		
+
 		}
 
 		template <>
 		void ICanvasT<Math::Geometry::Polyline>::draw(const Math::Geometry::Polyline& polyline) {
-		
+
 		}
 
 		template <>
 		void ICanvasT<Math::Geometry::Triangle>::draw(const Math::Geometry::Triangle& triangle) {
-		
+
 		}
 
 	}
